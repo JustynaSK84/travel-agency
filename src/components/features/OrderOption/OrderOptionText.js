443c17fd2ea61b80a26e01925c09e70aea83985f
Justyna Sadowska <korzyniewskaj@gmail.com>
2022-03-13 20:11:38 +0100

    19.4 send to API

--- src/components/features/OrderOption/OrderOptionText.js
+++ src/components/features/OrderOption/OrderOptionText.js
@@ -14,7 +14,6 @@ const OrderOptionText = ({setOptionValue}) => {
 };
 OrderOptionText.propTypes = {
   setOptionValue: PropTypes.func,
-  currentValue: PropTypes.string,
 };
 
     
--- src/components/features/OrderSummary/OrderSummary.js
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -4,23 +4,27 @@ import {calculateTotal} from '../../../utils/calculateTotal';
 import {formatPrice} from  '../../../utils/formatPrice';
 import PropTypes from 'prop-types';
 
-const OrderSummary = ({ tripCost, tripOptions }) => (
-  <div>
-    <h2 className={styles.component}>
-      Total:
-      <strong>
-        {formatPrice(
-          calculateTotal(tripCost, tripOptions),
-        )}
-      </strong>
-    </h2>
-  </div>
-);
+const OrderSummary = (props) => {
+  console.log(calculateTotal);
+  console.log(props, 'props');
+  return (
+    <div>
+      <h2 className={styles.component}>
+        Total:
+        <strong>
+          {formatPrice(
+            calculateTotal(props.cost, props.options),
+          )}
+        </strong>
+      </h2>
+    </div>
+  );
+};
 
 OrderSummary.propTypes = {
-  tripCost: PropTypes.string,
   options: PropTypes.object,
   tripOptions: PropTypes.object,
+  cost: PropTypes.string,
 };
 
 export default OrderSummary; 
