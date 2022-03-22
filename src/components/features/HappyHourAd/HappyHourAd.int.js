cc74b481abd5e1c478736ce9dbf72a6cde9e876d
Justyna Sadowska <korzyniewskaj@gmail.com>
2022-03-22 21:20:34 +0100

    20.3 Format Time

--- src/components/features/HappyHourAd/HappyHourAd.int.js
+++ src/components/features/HappyHourAd/HappyHourAd.int.js
@@ -11,43 +11,18 @@ const mockProps = {
   title: 'Header Title Lorem Ipsum',
   promoDescription: 'Promo Description Lorem Ipsum',
 };
-beforeAll(() => {
-  const utilsModule = jest.requireActual('../../../utils/formatTime.js');
-  utilsModule.formatTime = jest.fn(seconds => seconds);
-});
 
-describe('Component HappyHourAd', () => {
-  it('TEST 23: should render without crashing', () => {
-    const component = shallow(<HappyHourAd />);
-
-    expect(component).toBeTruthy();
-  });
-
-  it('TEST 24: contains H3 title and Promo Description', () => {
-    const component = shallow(<HappyHourAd />);
-    expect(component.exists(select.title)).toEqual(true);
-    expect(component.exists(select.promoDescription)).toEqual(true);
-
-  });
-
-  it('TEST 25: render correct header title', () => {
-    const component = shallow(<HappyHourAd {...mockProps} />);
-
-    expect(component.find(select.title).text()).toEqual(mockProps.title);
-
-  });
-});
 const trueDate = Date;
 const mockDate = customDate => class extends Date {
   constructor(...args) {
-    if(args.length){
+    if (args.length) {
       super(...args);
     } else {
       super(customDate);
     }
     return this;
   }
-  static now(){
+  static now() {
     return (new Date(customDate)).getTime();
   }
 };
@@ -68,28 +43,28 @@ const checkDescriptionAfterTime = (time, delaySeconds, expectedDescription) => {
   it(`TEST 27: should show correct value ${delaySeconds} after ${time}`, () => {
     jest.useFakeTimers();
     global.Date = mockDate(`2019-05-14T${time}.135Z`);
-  
+
     const component = shallow(<HappyHourAd {...mockProps} />);
     const newTime = new Date();
     // get date, as modified class -> return same hour every time 
-  
+
     newTime.setSeconds(newTime.getSeconds() + delaySeconds);
     // modify time by adding "delaySeconds" value
-  
+
     global.Date = mockDate(newTime.getTime());
     // replace Date for the new mock with changed time
-  
+
     jest.advanceTimersByTime(delaySeconds * 1000);
     // time accelerate *1000
-  
+
     const renderedTime = component.find(select.promoDescription).text();
     expect(renderedTime).toEqual(expectedDescription);
-  
+
     global.Date = trueDate;
     jest.useRealTimers();
   });
 };
-  
+
 describe('Component HappyHourAd with mocked Date', () => {
   checkDescriptionAtTime('11:57:58', '122');
   checkDescriptionAtTime('11:59:59', '1');
@@ -100,14 +75,4 @@ describe('Component HappyHourAd with mocked Date and delay', () => {
   checkDescriptionAfterTime('11:57:58', 2, '120');
   checkDescriptionAfterTime('11:59:58', 1, '1');
   checkDescriptionAfterTime('13:00:00', 60 * 60, 22 * 60 * 60 + '');
-});
-
-describe('Component HappyHourAd between 12:00:00 and 12:59:59', () => {
-  checkDescriptionAtTime('12:00:00', mockProps.promoDescription);
-  checkDescriptionAtTime('12:12:12', mockProps.promoDescription);
-  checkDescriptionAtTime('12:59:59', mockProps.promoDescription);
-});
-
-describe('Component HappyHourAd when the test started at 11:59:59 and the message will be dispayed after 12:00:00', () => {
-  checkDescriptionAfterTime('11:59:59', 1, mockProps.promoDescription);
 });
